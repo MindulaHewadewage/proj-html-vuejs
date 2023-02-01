0fc22c605e3a53baa7f93064df5cfab704449463
MindulaHewadewage <mindufernando@gmail.com>
2023-02-01 17:11:50 +0100

    struttura informazione date card

--- src/components/live-dates/LiveDateCard.vue
+++ src/components/live-dates/LiveDateCard.vue
@@ -9,8 +9,21 @@ export default {
 
 
 <template>
+    <!-- card -->
     <div class="live-date-card">
+        <font-awesome-icon icon="fa-solid fa-plus" />
         <p>{{ event.date }}{{ event.festival_name }}{{ event.location }}</p>
+    </div>
+
+    <!-- information -->
+    <div class="event-information">
+        <div class="event-map">
+            <img :src="event.location_img" alt="">
+        </div>
+        <div class="event-booking">
+            <h6>{{ event.festival_name }}</h6>
+            <p>{{ event.festival_description }}</p>
+        </div>
     </div>
 </template>
 <style scoped lang="scss">
@@ -18,14 +31,26 @@ export default {
     min-height: 50px;
     background-color: #323844;
     margin-bottom: 10px;
+    cursor: pointer;
 
     display: flex;
     align-items: center;
+
+
+    .fa-plus {
+        color: white;
+        padding-left: 20px;
+    }
 
     p {
         margin: 0;
         padding: 0 30px;
         color: #EC4858;
     }
+
+}
+
+.event-information {
+    display: flex;
 }
 </style>
--- src/components/live-dates/LiveDatesSection.vue
+++ src/components/live-dates/LiveDatesSection.vue
@@ -6,38 +6,39 @@ export default {
         return {
             events: [
                 {
+                    visible: false,
                     date: "17/08/2020 ",
                     festival_name: "GEM FESTIVAL 2020 ANAKALIA",
                     location: " - GEORGIA",
-                    location_img: "",
+                    location_img: "https://i.stack.imgur.com/fowQt.png",
                     festival_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quidem illo dolor fuga similique explicabo fugit dolorem dolorum consequuntur cum, beatae quisquam porro odio amet quos, eius corrupti ad debitis.",
                 },
                 {
                     date: "24/09/2020 ",
                     festival_name: "GROOVEFEST",
                     location: " - DOMINICAL REPUBLIC",
-                    location_img: "",
+                    location_img: "https://i.stack.imgur.com/fowQt.png",
                     festival_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quidem illo dolor fuga similique explicabo fugit dolorem dolorum consequuntur cum, beatae quisquam porro odio amet quos, eius corrupti ad debitis.",
                 },
                 {
                     date: "31/10/2020 ",
                     festival_name: "OASIS FESTIVAL 2020 MARRAKECH",
                     location: " - MOROCCO",
-                    location_img: "",
+                    location_img: "https://i.stack.imgur.com/fowQt.png",
                     festival_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quidem illo dolor fuga similique explicabo fugit dolorem dolorum consequuntur cum, beatae quisquam porro odio amet quos, eius corrupti ad debitis.",
                 },
                 {
                     date: "07/11/2020 ",
                     festival_name: "MOGA FESTIVAL",
                     location: " - ESSAOURIA,MOROCCO",
-                    location_img: "",
+                    location_img: "https://i.stack.imgur.com/fowQt.png",
                     festival_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quidem illo dolor fuga similique explicabo fugit dolorem dolorum consequuntur cum, beatae quisquam porro odio amet quos, eius corrupti ad debitis.",
                 },
                 {
                     date: "10/12/2020",
                     festival_name: "ENVISION FESTIVAL",
                     location: " - UVITA, COSTA RICA",
-                    location_img: "",
+                    location_img: "https://i.stack.imgur.com/fowQt.png",
                     festival_description: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quidem illo dolor fuga similique explicabo fugit dolorem dolorum consequuntur cum, beatae quisquam porro odio amet quos, eius corrupti ad debitis.",
                 },
 
@@ -67,5 +68,6 @@ export default {
 
 .small-container {
     height: 400px;
+    overflow-y: auto;
 }
 </style>
