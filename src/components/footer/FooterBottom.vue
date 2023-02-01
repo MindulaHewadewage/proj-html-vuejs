9b90f9459aa9c44626f436b5b25ad5fb22d2ee94
MindulaHewadewage <mindufernando@gmail.com>
2023-02-01 23:31:25 +0100

    footer bottom

--- src/components/footer/FooterBottom.vue
+++ src/components/footer/FooterBottom.vue
@@ -4,8 +4,50 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <div class="bg">
+        <div class="large-container">
+            <div class="copyright">
+                <ul>
+                    <a href="#">
+                        <li>@Copyright 2012 - 2020</li>
+                    </a>
+                    <a href="#">
+                        <li>AVADA THEME BY THEMEFUSION</li>
+                    </a>
+                    <a href="#">
+                        <li>ALL RIGHTS RESERVED</li>
+                    </a>
+                    <a href="#">
+                        <li>POWERED BY WORDPRESS</li>
+                    </a>
+                </ul>
+            </div>
+            <div class="social">
+                <a href="#"><font-awesome-icon icon="fa-brands fa-facebook-f" /></a>
+                <a href="#"><font-awesome-icon icon="fa-brands fa-twitter" /></a>
+                <a href="#"><font-awesome-icon icon="fa-brands fa-instagram" /></a>
+                <a href="#"><font-awesome-icon icon="fa-brands fa-youtube" /></a>
+            </div>
+        </div>
+    </div>
+</template>
 
-<style>
+<style scoped lang="scss">
+.large-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px 0;
 
+    .copyright {
+
+        ul {
+            display: flex;
+            margin: 0;
+            padding: 0;
+            font-size: 10px;
+        }
+    }
+}
 </style>
